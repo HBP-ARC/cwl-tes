02a7863ea2e03f409255c6202aabc7bda4cc3acf
Eleni Mathioulaki <emathioulaki@athenarc.gr>
2023-06-12 10:37:29 +0000

    specify py-tes version in setup.py

--- setup.py
+++ setup.py
@@ -42,11 +42,11 @@ setup(
     install_requires=[
         "cwltool==1.0.20191022103248",
         "future>=0.16.0",
-        "py-tes>=0.4.0",
+        "requests>=2.18.2",
+        "minio>=4.0.18",
+        "py-tes @ git+https://github.com/ohsu-comp-bio/py-tes@master#egg=py-tes",
         "PyJWT>=1.6.4",
-        "requests>=2.14.2",
         "typing_extensions>=3.7.4",
-        "minio>=4.0.18"
     ],
     extras_require={
         "test": [
